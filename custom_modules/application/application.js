4208f93a3ed1bb1b0906785b5e47c1de40be78b6
Alexander Oguzie-Ibeh <harlexibeh01@gmail.com>
2022-03-02 05:39:41 +0100

    add reset feature and save to local storage

--- custom_modules/application/application.js
+++ custom_modules/application/application.js
@@ -2,15 +2,22 @@ import NetworkGateway from '../gateway/gateway.js';
 
 class Application {
   constructor() {
-    this.gateWay = new NetworkGateway();
-    this.gateWay.getID('Seed');
-
     this.inputs = document.querySelectorAll('.userdetails');
     this.success_message = document.querySelector('.success-message');
     this.form = document.querySelector('.data-form');
     this.loadingAnime = document.querySelector('.loading_anime');
     this.tableList = document.querySelector('.table-content');
     this.refreshBtn = document.querySelector('.f5');
+    this.resetBtn = document.querySelector('.f4');
+    this.localStorageStringInstance = 'app_config';
+
+    this.gateWay = new NetworkGateway();
+    if (this.onLoad() != null) {
+      this.gateWay.id = this.onLoad();
+      this.refreshList();
+    } else {
+      this.createNewToken();
+    }
   }
 
   initApp = () => {
@@ -25,6 +32,26 @@ class Application {
       event.currentTarget.ref.refreshList();
     });
     this.refreshBtn.ref = this;
+
+    this.resetBtn.addEventListener('click', (event) => {
+      event.currentTarget.ref.createNewToken();
+    });
+    this.resetBtn.ref = this;
+  }
+
+  createNewToken = () => {
+    this.showLoader();
+    const res = this.gateWay.getID('Seed');
+    res.then((data) => {
+      const stringResult = data.result;
+      this.gateWay.id = stringResult.substring(13, 34);
+      this.onSave(this.gateWay.id);
+      this.hideLoader();
+      this.refreshList();
+    })
+      .catch((error) => {
+        throw error;
+      });
   }
 
   addUserScore = () => {
@@ -66,6 +93,12 @@ class Application {
         throw error;
       });
   }
+
+  onSave = (token) => {
+    localStorage.setItem(this.localStorageStringInstance, token);
+  }
+
+  onLoad = () => localStorage.getItem(this.localStorageStringInstance);
 
   showLoader = () => {
     this.loadingAnime.classList.remove('hide');
